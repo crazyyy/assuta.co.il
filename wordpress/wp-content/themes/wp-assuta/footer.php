0c329dd65a58c62e0db5a861601114836d359f68
crazyyy <crazyyy@gmail.com>
2016-05-01 18:12:23 +0300

    before work with homepage slider

---
+++ wordpress/wp-content/themes/wp-assuta/footer.php
@@ -0,0 +1,70 @@
+      <!-- footer -->
+      <tr valign="top">
+        <td class="home-footer">
+          <table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" class="copyright">
+            <tbody>
+              <tr>
+                <td nowrap="1" align="right">
+                  &copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?>.
+                </td>
+                <td nowrap="1" align="right" width="55%">
+                  <a target="_blank" href="#" class="copyright" rel="publisher">Google+</a> &nbsp;|&nbsp;
+                  <a class="copyright" href="#">Об "Ассуте"</a> &nbsp;|&nbsp;
+                  <a class="copyright" href="#">Отделение Медицинского туризма </a> &nbsp;|&nbsp;
+                </td>
+                <td nowrap="1" align="right" style="padding-left:5px;" width="26%">
+                </td>
+              </tr>
+              <tr>
+                <td colspan="6" align="center" style="padding-top:20px;padding-bottom:15px;">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/superbrands2015_logo.png" width="47" height="47" border="0" alt="superbrands" align="left">
+                  <a style="margin:0 50px;display:inline-block;" alt="" title="Ассута - крупнейший ведущий частный медицинский центр в Израиле" href="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/assutafotterlogo.gif" width="129" height="39" border="0" alt="" title="">
+                  </a>
+                  <a href="http://www.jointcommissioninternational.org/about-jci/jci-accredited-organizations/?c=IL" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-img-right.jpg" width="208" height="40" border="0" alt="JCI"></a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <?php wp_footer(); ?>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/idangerous.swiper.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/c_config.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/c_smartmenus.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/functionjs.js"></script>
+  <script>
+  showContent('11');
+  $(document).ready(function() {
+    // swiper2 - main
+    var mySwiper2 = new Swiper('.home-swiper2 .swiper-container', {
+      autoplay: 5000,
+      pagination: '.pagination2',
+      loop: true,
+      grabCursor: true,
+      paginationClickable: true
+    });
+  });
+  $(document).ready(setMenuSize());
+  var __anchorItem = null;
+  if (location.hash.substr(1) != "") {
+    try {
+      __anchorItem = document.getElementById(location.hash.substr(1));
+    } catch (e) {
+      try {
+        __anchorItem = document.all[location.hash.substr(1)];
+      } catch (e) {}
+    }
+  }
+  if (__anchorItem != null) {
+    try {
+      __anchorItem.style.border = "1px solid red";
+    } catch (e) {}
+  }
+  </script>
+
+</body>
+</html>
---
+++ wordpress/wp-content/themes/wp-assuta/header.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
+
+  <!-- icons -->
+  <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
+
+  <!--[if lt IE 9]>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/selectivizr.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/respond.js"></script>
+  <![endif]-->
+  <!-- css + javascript -->
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<a name="top"></a>
+<table width="1010" height="100%" align="center" cellpadding="0" cellspacing="0" border="0">
+  <tbody>
+    <tr>
+      <td>
+        <div class="headerok clearfix">
+          <div class="header_left_div">
+            <div class="AssutaMainTitle">
+              <h1><?php bloginfo( 'name' ); ?></h1>
+            </div>
+            <div class="header-container">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/JCI_logo50.jpg" style="float:right;margin-right: 10px;">
+              <table border="0" class="topmenuh" cellspacing="0" cellpadding="0" style="padding-top:20px;">
+                <tbody>
+                  <tr>
+                    <td>
+                      <table name="SearchBar" cellpadding="0" cellspacing="0" border="0" align="center">
+                        <tbody>
+                          <tr>
+                            <td valign="top">
+                              <form name="searchform" method="get" style="margin:0px;" action="<?php bloginfo('url'); ?>/">
+                              <input type="text" name="s" class="search_input" placeholder="Поиск">
+                            </td>
+                            <td>
+                              <input src="<?php echo get_template_directory_uri(); ?>/img/search_button1.gif" value="submit" type="image" border="0" alt="Поиск">
+                              </form>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td class="topMenu">28/04/2016</td>
+                    <td class="topMenu"></td>
+                    <td class="topMenu"></td>
+                    <td class="topMenu">
+                      <a href="#" target="_blank">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/new_fb_icon.png" border="0" alt="">
+                      </a>
+                    </td>
+                    <td class="topMenu">
+                      <a target="_blank" href="#">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/youtube.jpg" height="18" border="0" alt="">
+                      </a>
+                    </td>
+                    <td class="topMenu">
+                      <a target="_blank" href="#">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/vk.png" border="0" height="18" alt="">
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div><!-- header-container -->
+          </div><!-- header_left_div -->
+
+          <div class="header_right_div">
+            <a href="<?php echo home_url(); ?>/contact.htm">
+              <div class="contact_title">Консультация бесплатно</div>
+              <div class="contact_phone">+972-3-7643247</div>
+            </a>
+          </div><!-- header_right_div -->
+
+          <div class="top-header">
+            <div class="logo">
+              <?php if ( is_front_page() && is_home() ){ } else { ?>
+                <a href="<?php echo home_url(); ?>">
+              <?php  } ?>
+                <h2 class="top-logo">Монада Медикал Центр</h2>
+                <h3 class="top-description">Мы ценим доверие наших клиентов</h3>
+              <?php if ( is_front_page() && is_home() ){ } else { ?>
+                </a>
+              <?php } ?>
+            </div><!-- /logo -->
+            <?php wpeHeadNav(); ?>
+          </div><!-- top-header -->
+          <div class="header-bottom-menu">
+            <h5 class="header-bottom-descr">Лечение в Израиле</h5>
+            <?php wpeHeadNavBott(); ?>
+          </div><!-- /.header-bottom-menu -->
+        </div><!-- headerok -->
+<!-- header -->
